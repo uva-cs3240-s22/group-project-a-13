5d9baf2366038120069fe9c73c1a5535b7d057e2
Richw818 <richwang81818@gmail.com>
2022-02-24 16:27:34 -0500

    Added more setup to initial App

--- WordOfMouth/settings.py
+++ WordOfMouth/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'WordOfMouthApp.apps.WordofmouthappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
