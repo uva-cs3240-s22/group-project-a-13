f3045daba806340224e4c2e23dad0654af9e5c79
Richw818 <richwang81818@gmail.com>
2022-02-24 18:32:54 -0500

    Changed Base Message

--- WordOfMouthApp/views.py
+++ WordOfMouthApp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World!")
+    return HttpResponse("Hello, World! This is the start of Group 13's Word of Mouth Project.")
