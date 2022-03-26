3b96223a828f8189eccac1a82303161f229c8327
Sophia Masrour <sophiamasrour@Sophias-MacBook-Air.local>
2022-03-26 16:21:56 -0400

    login redirec (Sophia Masrour)

--- WordOfMouthApp/views.py
+++ WordOfMouthApp/views.py
@@ -5,4 +5,6 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def login(request):
     # return HttpResponse("Hello, World! This is the start of Group 13's Word of Mouth Project.")
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('recipes')
     return render(request, 'WordOfMouth/login.html')
