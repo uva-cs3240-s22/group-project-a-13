e919078949384329aa4a7d4575897bfa21f04490
Richw818 <richwang81818@gmail.com>
2022-04-06 02:13:23 -0400

    Changed recipe creation, added ability to add and remove fields and edit description

--- recipes/models.py
+++ recipes/models.py
@@ -4,11 +4,11 @@ from django.urls import reverse
 
 class Recipe(models.Model):
     recipe_name = models.CharField(max_length = 50, default = '')
+    recipe_description = models.CharField(max_length=1000, default = '')
     recipe_ingredients = models.CharField(max_length = 500, default = '')
     recipe_equipment = models.CharField(max_length = 500, default = '')
     recipe_instructions = models.CharField(max_length = 1000, default = '')
     recipe_image = models.ImageField(default='', upload_to='media/')
-    num_instructions = models.IntegerField(default = 0)
     recipe_reference = models.IntegerField(default = 0, null = True)
     favorites = models.ManyToManyField(User, related_name="favorites", blank=True)
    
@@ -26,8 +26,22 @@ class Recipe(models.Model):
 
 class RecipeInstruction(models.Model):
     recipe = models.ForeignKey(Recipe, on_delete = models.CASCADE)
-    instruction_text = models.CharField(max_length = 500)
+    instruction_text = models.CharField(max_length = 500, default = '')
 
     def __str__(self):
         return self.instruction_text
-    
+
+class RecipeEquipment(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    equipment_text = models.CharField(max_length = 100, default = '')
+
+    def __str__(self):
+        return self.equipment_text
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient_text = models.CharField(max_length = 100, default = '')
+    ingredient_quantity = models.CharField(max_length = 100, default = '')
+
+    def __str__(self):
+        return self.equipment_text
--- recipes/urls.py
+++ recipes/urls.py
@@ -10,7 +10,18 @@ urlpatterns = [
     path('long/', views.long, name = 'long'),
     path('<int:recipe_id>/fork/', views.fork, name = 'fork'),
     path('<int:recipe_id>/add/', views.add, name = 'add'),
+    path('<int:recipe_id>/add_e/', views.add_e, name = 'add_e'),
+    path('<int:recipe_id>/add_i/', views.add_i, name = 'add_i'),
+    path('<int:recipe_id>/add_instruction/', views.add_instruction, name = 'add_instruction'),
+    path('<int:recipe_id>/add_equipment/', views.add_equipment, name = 'add_equipment'),
+    path('<int:recipe_id>/add_ingredient/', views.add_ingredient, name = 'add_ingredient'),
+    path('<int:recipe_id>/edit_description/', views.edit_description, name = 'edit_description'),
+    path('<int:recipe_id>/edit_d/', views.edit_d, name = 'edit_d'),
     path('myrecipes/', views.myrecipes, name = 'myrecipes'),
     path('favorited_list/', views.favorited_list, name='favorited_list'),
-    path('favorite/', views.favorite_recipe, name='favorite_recipe')
+    path('favorite/', views.favorite_recipe, name='favorite_recipe'),
+    path('<int:instruction_id>/<int:recipe_id>/delete_instruction/', views.delete_instruction, name = 'delete_instruction'),
+    path('<int:equipment_id>/<int:recipe_id>/delete_equipment/', views.delete_equipment, name = 'delete_equipment'),
+    path('<int:ingredient_id>/<int:recipe_id>/delete_ingredient/', views.delete_ingredient, name = 'delete_ingredient'),
+
 ]
