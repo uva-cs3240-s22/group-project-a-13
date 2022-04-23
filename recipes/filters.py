7b3c901b587151857f6d382daa6875dcf44df753
Sophia Masrour <sophiamasrour@Sophias-MacBook-Air.local>
2022-04-23 01:03:00 -0400

    another debug

--- recipes/filters.py
+++
@@ -1,8 +0,0 @@
-from typing import OrderedDict
-import django_filters
-from .models import *
-
-class RecipeFilter(django_filters.FilterSet):
-    class Meta:
-        model = Order
-        fields = []
